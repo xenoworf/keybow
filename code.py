a0a7f03290127e1259ff74db733310894fa72b03
Elliott Hoffman <elliott.hoffman@cerner.com>
2022-06-16 08:48:04 -0500

    cool things

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-
 from pmk import PMK
 from pmk.platform.keybow2040 import Keybow2040 as Hardware
 
@@ -15,48 +14,72 @@ keys = keybow.keys
 keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(keyboard)
 
-colors = {
-    'red': (255, 0, 0),
-    'off': (0, 0, 0),
-    'yellow': (255, 255 // 2, 0),
-    'green': (0, 255, 0),
-    'blue': (0, 0, 255)
-}
-
-keycodes = {
-    'backspace': Keycode.BACKSPACE,
-    'control': Keycode.LEFT_CONTROL,
-    'delete': Keycode.DELETE,
-    'down': Keycode.DOWN_ARROW,
-    'f11': Keycode.F11,
-    'f12': Keycode.F12,
-    'f9': Keycode.F9,
-    'grave': Keycode.GRAVE_ACCENT,
-    'insert': Keycode.INSERT,
-    'left': Keycode.LEFT_ARROW,
-    'right': Keycode.RIGHT_ARROW,
-    'shift': Keycode.LEFT_SHIFT,
-    'space': Keycode.SPACEBAR,
-    'tab': Keycode.TAB,
-    'up': Keycode.UP_ARROW,
-    'y': Keycode.Y,
-    'z': Keycode.Z,
-}
-
 # A map of keycodes that will be mapped sequentially to each of the keys, 0-15
-# SunVox
-keymap =    [
-    ['f9'], ['delete'], ['control', 'y'], ['control', 'z'], # 0x0-3
-    ['left', 'f11'], ['grave'], ['left'], ['tab'], # 0x4-7
-    ['shift', 'f12'], ['insert'], ['down'], ['up'], # 0x8-B
-    [], ['backspace'], ['right'], ['space'] # 0xC-F
+keymap = [
+    ["f9"],
+    ["delete"],
+    ["control", "y"],
+    ["control", "z"],  # 0x0-3
+    ["left", "f11"],
+    ["grave"],
+    ["left"],
+    ["tab"],  # 0x4-7
+    ["shift", "f12"],
+    ["insert"],
+    ["down"],
+    ["up"],  # 0x8-B
+    [],
+    ["backspace"],
+    ["right"],
+    ["space"],  # 0xC-F
 ]
 
+keycodes = {
+    "backspace": Keycode.BACKSPACE,
+    "control": Keycode.LEFT_CONTROL,
+    "delete": Keycode.DELETE,
+    "down": Keycode.DOWN_ARROW,
+    "f11": Keycode.F11,
+    "f12": Keycode.F12,
+    "f9": Keycode.F9,
+    "grave": Keycode.GRAVE_ACCENT,
+    "insert": Keycode.INSERT,
+    "left": Keycode.LEFT_ARROW,
+    "right": Keycode.RIGHT_ARROW,
+    "shift": Keycode.LEFT_SHIFT,
+    "space": Keycode.SPACEBAR,
+    "tab": Keycode.TAB,
+    "up": Keycode.UP_ARROW,
+    "y": Keycode.Y,
+    "z": Keycode.Z,
+}
+
+
+colors = {
+    "red": (255, 0, 0),
+    "off": (0, 0, 0),
+    "yellow": (255, 255 // 2, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+}
+
 rgb_map = [
-    'red', 'off', 'yellow', 'yellow', # 0-3
-    'off', 'green', 'blue', 'off', # 4-7
-    'off', 'green', 'blue', 'blue', # 8-B
-    'red', 'off', 'blue', 'off' # C-F
+    "red",
+    "off",
+    "yellow",
+    "yellow",  # 0-3
+    "off",
+    "green",
+    "blue",
+    "off",  # 4-7
+    "off",
+    "green",
+    "blue",
+    "blue",  # 8-B
+    "red",
+    "off",
+    "blue",
+    "off",  # C-F
 ]
 
 for key in keys:
@@ -65,8 +88,20 @@ for key in keys:
 
     @keybow.on_press(key)
     def press_handler(key):
-        codes = keymap[key.number]
-        [keyboard.send(keycodes[c]) for c in codes]
+        mapping = keymap[key.number]
+        codes = [keycodes[c] for c in mapping]
+        print(f"Press {mapping} {codes}")
+        keyboard.send(*codes)
+
+    @keybow.on_release(key)
+    def release_handler(key):
+        mapping = keymap[key.number]
+        print(f"Release {mapping}")
+
+    @keybow.on_hold(key)
+    def hold_handler(key):
+        mapping = keymap[key.number]
+        print(f"Hold {mapping}")
 
 while True:
     keybow.update()
